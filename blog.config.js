1af8533af57c3109efa17702fda8642eb1ac6f0d
tiandingyi <51780848+tiandingyi@users.noreply.github.com>
2023-05-30 18:14:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: '人间的分贝',
+  author: '人间的分贝',
+  email: 'fujianeric@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
