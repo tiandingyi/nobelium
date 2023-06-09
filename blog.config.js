a0a47f82d4294cf6b2f6b5357383e9b12cde6ad2
tiandingyi <51780848+tiandingyi@users.noreply.github.com>
2023-06-09 14:52:57 +0800

    update blog config

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/craigaryhart',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', '人间的分贝', 'Notion', 'SRE','Devops', 'cloud'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
